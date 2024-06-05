438618a9bf58ccd41855e406c730f71ef1e7a6d3
CARLOS ANTONIO ANGULO ARIAS <130503712+vjcarlosedit@users.noreply.github.com>
2024-06-04 19:41:48 -0600

    main.py

--- main.py
+++ main.py
@@ -2,30 +2,49 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.theme_mode = ft.ThemeMode.LIGHT
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+    page.title = "Sistema Posgrado"
+    page.theme_mode = "light"
+    appBar = ft.AppBar(title=ft.Text("Sistema De Adminstración"), 
+                       center_title=True, 
+                       bgcolor="green", 
+                       color="white")
+    page.appbar = appBar
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    logo_ujat = ft.Image(src="logo-ujat.png", width=200)
+    filaLogo = ft.Row([logo_ujat], alignment="center")
+        
+    txtBienvenido = ft.Text("¡Bienvenido!", size=24, text_align="center", width=page.window_width, color="green")
+    
+    txtDatos = ft.Text("Av. Universidad s/n, Zona de la Cultura, Col. Magisterial, Vhsa, Centro, Tabasco, Mex. C.P. 86040. Tel (993) 358 15 00", size=10, text_align="center", width=page.window_width, color="green", height=200)
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
+    btnNuevoPosgrado = ft.NavigationRailDestination(label="Nuevo Posgrado", icon="add_circle_outline", selected_icon="add_circle")
+    
+    btnNuevoResponsable = ft.NavigationRailDestination(label="Nuevo Responsable", icon="person_add_outlined", selected_icon="person_add_rounded")
+    
+    btnBajaPosgrado = ft.NavigationRailDestination(label="Bajas De Posgrados", icon="ARCHIVE_OUTLINED", selected_icon="ARCHIVE")
+    
+    btnReportesPosgrado = ft.NavigationRailDestination(label="Reportes Posgrados", icon="TABLE_CHART_OUTLINED", selected_icon="TABLE_CHART_ROUNDED")
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
+    btnReportesResponsable = ft.NavigationRailDestination(label="Reportes Responsables", icon="TABLE_CHART_OUTLINED", selected_icon="TABLE_CHART_ROUNDED")
 
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
+    btnReportesDivision = ft.NavigationRailDestination(label="Reportes División", icon="TABLE_CHART_OUTLINED", selected_icon="TABLE_CHART_ROUNDED")
+    
+    listaBotones = [btnNuevoPosgrado, btnNuevoResponsable, btnBajaPosgrado, btnReportesPosgrado, btnReportesResponsable, btnReportesDivision]
 
+    contenido = ft.Column(
+        [txtBienvenido,filaLogo,txtDatos], 
+        # horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        alignment="center",
+        spacing=50
+    )    
+    
+    navRail = ft.NavigationRail(bgcolor="green50", destinations=listaBotones) # on_change=mostrar_opcion
+    contenedorPrincipal = ft.Container(content=contenido, expand=True)
+    
+    fila = ft.Row([navRail, contenedorPrincipal], expand=True)
+    
+    page.add(fila)
+    page.update()    
 
 ft.app(main, view=ft.AppView.WEB_BROWSER)
