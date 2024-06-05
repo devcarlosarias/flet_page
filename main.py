8a278632c3bf9b4fc22f02789c79ad866760533f
CARLOS ANTONIO ANGULO ARIAS <130503712+vjcarlosedit@users.noreply.github.com>
2024-06-04 20:41:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,28 @@ def main(page: ft.Page):
                        color="white")
     page.appbar = appBar
     
-    logo_ujat = ft.Image(src="icon.png", width=200)
+    logo_ujat = ft.Image(src="https://github.com/vjcarlosedit/flet_page/raw/main/assets/logo-ujat.png", width=200)
     filaLogo = ft.Row([logo_ujat], alignment="center")
-
-
-
     
-    txtBienvenido = ft.Text("¡Bienvenido! Hola Flet")
+    txtBienvenido = ft.Text("¡Bienvenido!", size=24, text_align="center", width=page.window_width, color="green")
+    txtDatos = ft.Text("Av. Universidad s/n, Zona de la Cultura, Col. Magisterial, Vhsa, Centro, Tabasco, Mex. C.P. 86040. Tel (993) 358 15 00", size=10, text_align="center", width=page.window_width, color="green", height=200)
 
+    btnNuevoPosgrado = ft.NavigationRailDestination(label="Nuevo Posgrado", icon="add_circle_outline", selected_icon="add_circle")
+    btnNuevoResponsable = ft.NavigationRailDestination(label="Nuevo Responsable", icon="person_add_outlined", selected_icon="person_add_rounded")
+    btnBajaPosgrado = ft.NavigationRailDestination(label="Bajas De Posgrados", icon="ARCHIVE_OUTLINED", selected_icon="ARCHIVE")
+    btnReportesPosgrado = ft.NavigationRailDestination(label="Reportes Posgrados", icon="TABLE_CHART_OUTLINED", selected_icon="TABLE_CHART_ROUNDED"
+    btnReportesResponsable = ft.NavigationRailDestination(label="Reportes Responsables", icon="TABLE_CHART_OUTLINED", selected_icon="TABLE_CHART_ROUNDED")
+    btnReportesDivision = ft.NavigationRailDestination(label="Reportes División", icon="TABLE_CHART_OUTLINED", selected_icon="TABLE_CHART_ROUNDED")
+    
+    listaBotones = [btnNuevoPosgrado, btnNuevoResponsable, btnBajaPosgrado, btnReportesPosgrado, btnReportesResponsable, btnReportesDivision]
+    
     contenido = ft.Column(
-        [txtBienvenido,filaLogo], 
+        [txtBienvenido,filaLogo,txtDatos], 
         alignment="center",
         spacing=50
     )    
 
+    navRail = ft.NavigationRail(bgcolor="green50", destinations=listaBotones)
     contenedorPrincipal = ft.Container(content=contenido, expand=True)
     fila = ft.Row([navRail, contenedorPrincipal], expand=True)    
     
